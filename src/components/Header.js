b4d7dd5724e0b3bbcc1e28039e52e820e59dd730
Allison Zentmayer <akz@vt.edu>
2019-03-22 16:32:40 -0400

    removing comment

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import './Header.css'
 export class Header extends Component {
     render() {
       return (
-          // add home button
         <header className="header">
             <div className="header-text" onClick={() => this.props.clearState()}>
                 Home
